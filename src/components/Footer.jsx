2e1e3717d4c0d0b7cfbfdddb11225f307ffd9cd3
Jackie Manginelli <jmanginelli11@gmail.com>
2025-07-12 18:00:47 -0400

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const Footer = () => {
+  return (
+    <>
+      <div>
+        <p className='read-the-docs'>Made with the best ingredients:</p>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Footer; 
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <>
+        <h1>Jackie Manginelli (they/them)</h1>
+        </>
+    );
+}
+
+export default Header; 
